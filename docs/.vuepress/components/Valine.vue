2a481f675c903582c07263ac49243ee42f83632d
ynzy <758922096@qq.com>
2019-04-22 16:42:42 +0800

    增加valine评论模块

---
+++ docs/.vuepress/components/Valine.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="vcomments"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    this.renderValine();
+  },
+  methods: {
+    renderValine() {
+      if (typeof window !== "undefined") {
+        this.window = window;
+        window.AV = require("leancloud-storage");
+      }
+      const Valine = require('valine');
+      new Valine({
+        el: "#vcomments",
+        appId: "ARV3PfjCJG8mKiTrEDnsowwF-gzGzoHsz",
+        appKey: "64oXlEAQT9aJvvzipcgHLjpf",
+        notify: false,
+        verify: false,
+        avatar: "retro",
+        placeholder: "欢迎留言"
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
--- docs/about/README.md
+++ docs/about/README.md
@@ -6,3 +6,7 @@ navbar: false
 
 vuepress更新了升级->1.0
 vuepress1.0x
+
+增加评论哈哈哈增加了
+
+<Valine/>
